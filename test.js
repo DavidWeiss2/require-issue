9cb3a20499628c18bb7c85f43d7176db87c1fdf5
David Weiss <DavidWeiss2@gmail.com>
2022-09-15 21:00:09 +0300

    workaround

--- test.js
+++ test.js
@@ -1,17 +1,26 @@
-const fs = require('fs');
-const path = require('path');
-try { require('child_process').execSync("rm -rf node_modules"); } catch (err) {}
-try { fs.mkdirSync('node_modules'); } catch (err) {}
+const fs = require("fs");
+const path = require("path");
 try {
-  require('pkg');
+  require("child_process").execSync("rm -rf node_modules");
+} catch (err) {}
+try {
+  fs.mkdirSync("node_modules");
+} catch (err) {}
+try {
+  require("pkg");
 } catch (err) {
-  console.log('failed requiering pkg');
+  console.log("failed requiering pkg");
 }
 
-try { require('child_process').execSync("cp -r " + path.join(__dirname, 'pkg') + " node_modules/pkg"); } catch (err) {}
-
 try {
-  require('pkg');
-} catch (err) {
-  console.log('failed requiering pkg');
-}
+  require("child_process").execSync(
+    "cp -r " + path.join(__dirname, "pkg") + " node_modules/pkg"
+  );
+} catch (err) {}
+
+new Promise(() =>
+  setTimeout(() => {
+    require("pkg");
+    console.log("works");
+  }, 0)
+).catch((e) => console.log("failed requiering pkg", e));
